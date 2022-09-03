6e5afce261d0658d31ccfed8448900b52b2ea6b6
Jaemyeong Jin <jaemyeong@me.com>
2022-09-03 11:27:14 +0900

    Tailwind CSS

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,10 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>안녕하세요!</h1>
+    <div>
+      <BeakerIcon className="h-6 w-6 text-blue-500"/>
+      <h1 className="text-3xl font-bold underline">안녕하세요!</h1>
     </div>
   );
 }
